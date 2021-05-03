0c5517c2db6afef52dcf3ad0cb45a84b6f68342e
James Moore <james.p.development@gmail.com>
2021-05-03 12:01:29 +1000

    theme based splash screen

---
+++ android/app/src/main/kotlin/com/example/flutterlaunch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterlaunch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/rl/launch/flutterlaunch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rl.launch.flutterlaunch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
